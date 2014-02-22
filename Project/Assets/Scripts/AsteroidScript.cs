213357a3ecc83e39736836f1c276c4241609c15d
Michael Tesija <mtesija@umich.edu>
2014-02-21 23:49:57 -0500

    spawners

---
+++ Project/Assets/Scripts/AsteroidScript.cs
@@ -0,0 +1,65 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AsteroidScript : MonoBehaviour 
+{
+	private float gravity = 10;
+
+	private GameObject cam;
+	private GameObject player;
+
+	void Start()
+	{
+		cam = GameObject.Find("Main Camera");
+		player = GameObject.Find("Player");
+	}
+
+	void Update()
+	{
+		Gravity();
+
+		Vector3 position = cam.transform.position;
+		position.y = 0;
+
+		if(Vector3.Distance(position, this.transform.position) > 200)
+		{
+			Destroy(this.gameObject);
+		}
+	}
+
+	void Gravity()
+	{
+		Vector3 direction = player.transform.position - this.transform.position;
+		direction.Normalize();
+
+		float distance = Vector3.Distance(player.transform.position, this.transform.position);
+
+		direction *= gravity * player.rigidbody.mass / (distance * distance);
+
+		this.rigidbody.AddForce(direction);
+	}
+
+	void OnCollisionEnter(Collision coll)
+	{
+		if(coll.gameObject.name == "Player")
+		{
+			Destroy(this.gameObject);
+		}
+		else if(coll.gameObject.name == "Asteroid(Clone)")
+		{
+			if(this.transform.localScale.x > coll.gameObject.transform.localScale.x)
+			{
+				Vector3 thisVelocity = this.rigidbody.velocity * this.rigidbody.mass;
+				Vector3 collVelocity = coll.rigidbody.velocity * coll.rigidbody.mass;
+				float newMass = this.rigidbody.mass + coll.rigidbody.mass;
+				Vector3 newVelocity = (thisVelocity + collVelocity) / (newMass);
+
+				Destroy(coll.gameObject);
+
+				this.rigidbody.mass = newMass;
+				this.rigidbody.velocity = newVelocity;
+				this.transform.localScale = Vector3.one * Mathf.Pow(newMass, 1f / 3f);
+			}
+		}
+	}
+}
---
+++ Project/Assets/Scripts/AsteroidSpawnerScript.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AsteroidSpawnerScript : MonoBehaviour {
+
+	public GameObject Asteroid;
+
+	public float lowAngle = 0;
+	public float highAngle = Mathf.PI;
+
+	public float minVelocity = 0;
+	public float maxVelocity = 8;
+
+	public float minSize = 1f;
+	public float maxSize = 3f;
+
+	public float minTime = 1.7f;
+	public float maxTime = 5.2f;
+
+	private float spawnTimer = 0;
+
+	void Start()
+	{
+		spawnTimer = Random.Range(minTime, maxTime);
+	}
+
+	void Update() 
+	{
+		spawnTimer -= Time.deltaTime;
+
+		if(spawnTimer <= 0)
+		{
+			spawnTimer = Random.Range(minTime, maxTime);
+			spawnAsteroid();
+		}
+	}
+
+	void spawnAsteroid()
+	{
+		GameObject asteroid = Instantiate(Asteroid, this.transform.position, Quaternion.identity) as GameObject;
+
+		float angle = Random.Range(lowAngle, highAngle);
+		float velocity = Random.Range(minVelocity, maxVelocity);
+		float size = Random.Range(minSize, maxSize);
+
+		asteroid.rigidbody.velocity = new Vector3(Mathf.Cos(angle), 0 , Mathf.Sin(angle)) * velocity;
+		asteroid.transform.localScale = Vector3.one * size;
+		asteroid.rigidbody.mass = size * 20;
+	}
+}
