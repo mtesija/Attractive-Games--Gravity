325f40f692e38c2b5fb7e90cfaed90c90e04e4a7
codykraz <bassguitarninja@gmail.com>
2014-02-22 22:49:55 -0500

    Title Screen

--- Project/Assets/Scripts/AsteroidScript.cs
+++ Project/Assets/Scripts/AsteroidScript.cs
@@ -69,7 +69,6 @@ public class AsteroidScript : MonoBehaviour
 		if(coll.gameObject.name == "Player")
 		{
 			Destroy(this.gameObject);
-			Application.LoadLevel("_Scene1");
 		}
 		else if(coll.gameObject.name == "Asteroid(Clone)")
 		{
